bdaac1c81fa5c4aed623bc3f02d3cbc3881bbeca
Pulkit Aggarwal <pulkit.aggarwal@pulkita-ltmam3w.internal.salesforce.com>
2024-06-23 08:06:56 +0530

    Code Updated with output

---
+++ MinMaxNumber.js
@@ -0,0 +1,17 @@
+// Find Min number from an array/list
+
+const array = [1,2,3,4,5,6,-3,99];
+
+let min = array[0];
+let max = array[0];
+
+for(let i = 0; i < array.length; i++){
+    if(array[i] < min){ min = array[i]; }
+    
+    if(array[i] > max){ max = array[i]; }
+}
+
+console.log('min = '+min)
+console.log('max = '+max)
+
+// output: min = -3 max = 99
